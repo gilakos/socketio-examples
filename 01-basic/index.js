d37605eb00aed9f49fbbfffbccc73de311ab1127
Gil Akos <gil@astra.finance>
2016-12-03 20:29:23 -0600

    01-Step 01
    Create package and basic index.js files using express and socket

---
+++ 01-basic/index.js
@@ -0,0 +1,14 @@
+//Express initializes app to be a function handler that you can supply to an HTTP server.
+var app = require('express')();
+//Create an HTTP server.
+var http = require('http').Server(app);
+
+//We define a route handler / that gets called when we hit our website home.
+app.get('/', function(req, res){
+  res.send('<h1>Hello world</h1>');
+});
+
+//We make the http server listen on port 3000.
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
