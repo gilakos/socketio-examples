936ef3ea6831755f17f7caac457c95afa0ef74b5
Gil Akos <gil@astra.finance>
2016-12-03 20:47:19 -0600

    03-Integrating Socket.IO
    Add socketio client and connection event function

--- 01-basic/index.js
+++ 01-basic/index.js
@@ -2,6 +2,8 @@
 var app = require('express')();
 //Create an HTTP server.
 var http = require('http').Server(app);
+//Create instance of socket
+var io = require('socket.io')(http);
 
 //We define a route handler / that gets called when we hit our website home.
 app.get('/', function (req, res) {
@@ -9,6 +11,12 @@ app.get('/', function (req, res) {
     //Let’s refactor our route handler to use sendFile instead:
     res.sendFile(__dirname + '/index.html');
 });
+
+//Listen on the connection event for incoming sockets, and log it to the console.
+io.on('connection', function(socket){
+  console.log('a user connected');
+});
+
 
 //We make the http server listen on port 3000.
 http.listen(3000, function () {
